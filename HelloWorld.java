7b86faffbaff6ffc0413cba6d441aed9b9bcde4f
Naveen Nelogal <nelogalnaveen88@gmail.com>
2023-02-21 10:39:12 +0530

    changed

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld{
-  public static void main(String[] args){
-    System.out.println("Naveen s n");
-    System.out.println("changed file");
-
-  }
-}
--- second.java
+++
@@ -1,8 +0,0 @@
-//This is second program
-class Hi
-{
-	public static void main(String[] args)
-	{	
-		System.out.println("second file");
-	}
-}
